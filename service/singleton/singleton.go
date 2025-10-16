5c539cbadf38196b058b6c6eb9b1a3775a485607
TwoOnefour <lys214412@gmail.com>
2025-10-16 17:38:58 +0800

    fix(sqlite): 调整sqlite性能配置

--- service/singleton/singleton.go
+++ service/singleton/singleton.go
@@ -7,12 +7,12 @@ import (
 	"strconv"
 	"time"
 
+	go_sqlite "github.com/glebarez/sqlite"
 	"github.com/naiba/nezha/model"
 	"github.com/naiba/nezha/pkg/utils"
 	"github.com/patrickmn/go-cache"
 	"github.com/soheilhy/cmux"
 	"gorm.io/driver/mysql"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -23,7 +23,6 @@ var (
 	Cache *cache.Cache
 	DB    *gorm.DB
 	Loc   *time.Location
-	Cmux  *cmux.CMux
 )
 
 func InitTimezoneAndCache() {
@@ -58,8 +57,16 @@ func InitConfigFromPath(path string) {
 // InitDBFromPath 从给出的文件路径中加载数据库
 func InitDBFromPath(path string) {
 	var err error
-	DB, err = gorm.Open(sqlite.Open(path), &gorm.Config{
-		CreateBatchSize: 200,
+	path = path + "?_pragma=journal_mode(WAL)" +
+		"&_pragma=synchronous(NORMAL)" +
+		"&_pragma=busy_timeout(5000)" +
+		"&_pragma=cache_size(-32768)" +
+		"&_pragma=temp_store(memory)" +
+		"&_pragma=foreign_keys(ON)"
+	DB, err = gorm.Open(go_sqlite.Open(path), &gorm.Config{
+		CreateBatchSize:                          128,
+		SkipDefaultTransaction:                   true,
+		DisableForeignKeyConstraintWhenMigrating: true,
 	})
 	if err != nil {
 		panic(err)
